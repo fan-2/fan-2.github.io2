3f788697c633b0d9bfd4e980c79167fc7c1163ae
张帆 <farris.zhang@outlook.com>
2020-05-01 12:12:25 +0800

    imy

---
+++ config-overrides.js
@@ -0,0 +1,28 @@
+//覆盖默认webpack打包配置
+const {override, fixBabelImports, addLessLoader} = require("customize-cra");
+
+const path = require("path");
+//配置sourceMap
+const sourceMapConfig = config => {
+    config.devtool = "cheap-module-source-map";
+    return config;
+};
+
+//配置打包输入位置
+const outputConfig = config => {
+    config.output.path = path.resolve(__dirname, "./");
+    return config;
+}
+module.exports = override(
+    outputConfig,
+    sourceMapConfig,
+    fixBabelImports('import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true
+    }),
+    addLessLoader({
+        javascriptEnabled: true,
+        // modifyVars: {'@primary-color': '#ff8727'}  //这里用来配置主题颜色
+    })
+);
---
+++ precache-manifest.ef837cc2a72017a9b869e980243a2ebb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f9680523ea73648c8e9185bb1599bd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "241d241994b2bff78e1f",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d34df1abc64111f5a719",
+    "url": "/static/js/2.dfe8e32c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dfe8e32c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "241d241994b2bff78e1f",
+    "url": "/static/js/main.2f24ba4e.chunk.js"
+  },
+  {
+    "revision": "10cee05dd2961b19477e",
+    "url": "/static/js/runtime-main.6d50b040.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
